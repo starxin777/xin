6e7dfd5ced1d80076138afd731922daf3d6c6334
starxin777 <2965268137@qq.com>
2021-04-21 10:28:19 +0800

    rem移动端适配

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="home">
-    <van-button type="primary">默认按钮</van-button>
+    <div>你好</div>
   </div>
 </template>
 
 <script>
-
-
 export default {
   name: 'Home',
- 
-}
+};
 </script>
+<style lang="less" scoped>
+.home {
+  div{
+    background: grey;
+    width: 150px;
+    height: 150px;
+    font-size: 18px;
+  }
+}
+</style>
