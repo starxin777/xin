6f73bac1ac546aff5bb9edccae2cc634b9a45605
starxin777 <2965268137@qq.com>
2021-04-25 16:34:40 +0800

    轮播图

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,120 @@
 <template>
   <div class="home">
-    <div>我是首页</div>
+    <header class="home-header wrap">
+      <router-link tag="i" to="./category"
+        ><i class="nbicon nbmenu2"></i
+      ></router-link>
+      <div class="header-search">
+        <span class="app-name">楼楼商城</span>
+        <i class="iconfont icon-search"></i>
+        <router-link tag="span" class="search-title" to="./product-list"
+          >山河无恙，人间皆安</router-link
+        >
+      </div>
+      <router-link class="login" tag="span" to="./login" v-if="!isLogin"
+        >登录</router-link
+      >
+      <router-link class="login" tag="span" to="./user" v-else>
+        <van-icon name="manager-o" />
+      </router-link>
+    </header>
+    <swiper :list="swiperList"></swiper>
   </div>
 </template>
 
 <script>
-export default { name: "Home" };
+import { getLocal } from "@/common/js/utils";
+import swiper from "@/components/Swiper";
+import { getHome } from "../service/home";
+import { Toast } from "vant";
+export default {
+  name: "Home",
+  data() {
+    return {
+      isLogin: false,
+      swiperList: [],
+    };
+  },
+  components: {
+     swiper,
+  },
+  async mounted() {
+    const token = getLocal("token");
+    if (token) {
+       this.isLogin = true;
+    }
+    Toast.loading({
+      message: "加载中...",
+      forbidClick: true,
+  });
+  const { data } = await getHome();
+   this.swiperList = data.carousels;
+  },
+};
 </script>
 <style lang="less" scoped></style>
+
+<style lang="less" scoped>
+@import "../common/style/mixin";
+.home {
+  .home-header {
+    position: fixed;
+    left: 0;
+    top: 0;
+    .wh(100%, 50px);
+    .fj();
+    line-height: 50px;
+    padding: 0 15px;
+    .boxSizing();
+    font-size: 15px;
+    color: #fff;
+    z-index: 10000;
+    .icon-menu {
+      color: @primary;
+    }
+    &.active {
+      background: @primary;
+      .icon-menu {
+        color: #fff;
+      }
+      .login {
+        color: #fff;
+      }
+    }
+    .header-search {
+      display: flex;
+      .wh(74%, 20px);
+      line-height: 20px;
+      margin: 10px 0;
+      padding: 5px 0;
+      color: #232326;
+      background: rgba(255, 255, 255, 0.7);
+      border-radius: 20px;
+      .app-name {
+        padding: 0 10px;
+        color: @primary;
+        font-size: 20px;
+        font-weight: bold;
+        border-right: 1px solid #666;
+      }
+      .icon-search {
+        padding: 0 10px;
+        font-size: 17px;
+      }
+      .search-title {
+        font-size: 12px;
+        color: #666;
+        line-height: 21px;
+      }
+    }
+    .login {
+      color: @primary;
+      line-height: 52px;
+      .van-icon-manager-o {
+        font-size: 20px;
+        vertical-align: -3px;
+      }
+    }
+  }
+}
+</style>
